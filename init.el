a895981c4c89ca75fa043a95c1233364ab37468e
Francisco Dibar <frandibar@gmail.com>
2011-08-08 22:33:31 -0300

    added which-function-mode

--- init.el
+++ init.el
@@ -141,3 +141,18 @@ http://stackoverflow.com/questions/5194417/how-to-mark-the-text-between-the-pare
 
 (add-to-list 'load-path "~/.emacs.d/ecb-snap")
 (require 'ecb)
+
+(which-function-mode t)
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(ecb-options-version "2.40"))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
