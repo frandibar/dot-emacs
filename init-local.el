e90c989e82f464ceb53b48af3a5d3506b0aaf25e
Francisco Dibar <frandibar@gmail.com>
2011-11-26 12:10:00 -0300

    added sql opts

--- init-local.el
+++ init-local.el
@@ -6,3 +6,20 @@
 ;; use chrome as default browser
 (setq browse-url-browser-function 'browse-url-generic
       browse-url-generic-program "google-chrome")
+
+(eval-after-load "sql"
+  '(progn
+     (sql-set-product 'mysql)
+     (setq sql-server "localhost")
+     (setq sql-user "root")
+     (setq sql-password "root")
+     (setq sql-database "m2000")
+     (sql-connect-mysql)
+
+     ;; TODO fix, not working
+     ;; (require 'sql-completion)
+     ;; (setq sql-interactive-mode-hook
+     ;;       (lambda ()
+     ;;         (define-key sql-interactive-mode-map "\t" 'comint-dynamic-complete)
+     ;;         (sql-mysql-completion-init)))
+  ))
