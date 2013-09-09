8229aa247d5168dce051b89a05ab45958e707a78
Francisco Dibar <frandibar@gmail.com>
2013-09-09 09:31:44 -0300

    dired bindings

--- init.el
+++ init.el
@@ -426,13 +426,13 @@
     (setq dired-recursive-deletes (quote top))
 
     ;; ;; make Enter and ^ (parent dir) to use the same buffer
-    (add-hook 'dired-mode-hook
-              (lambda ()
-                (define-key dired-mode-map (kbd "<return>")
-                  'dired-find-alternate-file)               ; was dired-advertised-find-file
-                (define-key dired-mode-map (kbd "^")
-                  (lambda () (interactive) (find-alternate-file ".."))) ; was dired-up-directory
-                ))
+    ;; (add-hook 'dired-mode-hook
+    ;;           (lambda ()
+    ;;             (define-key dired-mode-map (kbd "<return>")
+    ;;               'dired-find-alternate-file)               ; was dired-advertised-find-file
+    ;;             (define-key dired-mode-map (kbd "^")
+    ;;               (lambda () (interactive) (find-alternate-file ".."))) ; was dired-up-directory
+    ;;             ))
 
 
     ;; use M-o to toggle viewing files
