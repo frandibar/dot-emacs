6a5ca7c3f5ef2bc0da9d8f7ddf3038aca7097585
Francisco Dibar <frandibar@gmail.com>
2014-11-06 11:50:20 -0300

    Disabled module elpy-module-highlight-indentation

--- emacs-custom.el
+++ emacs-custom.el
@@ -3,7 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(canlock-password "2f3810fc7958594bdec18e238ab18b227494fac4"))
+ '(canlock-password "2f3810fc7958594bdec18e238ab18b227494fac4")
+ '(elpy-modules
+   (quote
+    (elpy-module-company elpy-module-eldoc elpy-module-flymake elpy-module-pyvenv elpy-module-yasnippet elpy-module-sane-defaults))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
